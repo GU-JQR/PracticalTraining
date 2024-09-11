66c541e00925ab58074c0e425a69ae3f1898db91
GU-JQR <20210312213@squ.edu.cn>
2024-09-11 16:32:23 +0800

    hello vite

---
+++ Vue/day3/hello/src/components/First.vue
@@ -0,0 +1,14 @@
+/* eslint-disable */
+<template>
+    <div>
+        <h2>这是我的第一个组件</h2>
+        <img src="assets/food.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped></style>

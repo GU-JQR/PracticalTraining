02646c888558c7b2625188204ab7e7542c65a318
guclassmate <20210312213@squ.edu.cn>
2024-09-12 17:07:22 +0800

    axios

--- Vue/day3/hello/src/components/First.vue
+++ Vue/day3/hello/src/components/First.vue
@@ -3,11 +3,51 @@
     <div>
         <h2>这是我的第一个组件</h2>
         <img src="assets/food.jpg" alt="">
+        账号: <input type="text" v-model="number"><br>
+        密码: <input type="text" v-model="pwd"><br>
+        <button @click="login()">登录</button>
+        <br>
+        <button @click="sendPost()">发送ajax请求</button>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            number: '',
+            pwd: '',
+            result: '',
+            user: {
+                name: '小美',
+                age: 18,
+                address: '南京小行路16路',
+                phone: '110-119'
+            }
+        }
+    },
+    methods: {
+        login() {
+            axios.get('http://localhost:9090/atm/login.do', { params: { number: this.number, pwd: this.pwd } })
+                .then((response) => {
+                    this.result = response.data;
+                })
+                .catch((response) => {
+                    console.log(response)
+                })
+        },
+        sendPost() {
+            axios.post('http://localhost:9090/atm/login.do', this.user)
+                .then((response) => {
+                    this.result = response.data;
+                })
+        }
+
+
+    },
+    mounted: {
+
+    },
 }
 </script>
 

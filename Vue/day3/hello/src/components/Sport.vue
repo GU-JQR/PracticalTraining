d14b7cd988fe3bbd131f45379d039343efa89a59
guclassmate <20210312213@squ.edu.cn>
2024-09-13 16:21:53 +0800

    路由

---
+++ Vue/day3/hello/src/components/Sport.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-link to="/sport/football/2">男足</router-link>
+        <router-link to="/sport/nba?name=james">NBA</router-link>
+        <br>
+        男足
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
